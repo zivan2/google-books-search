ac205c10be3fae3d1373bf83b97916215c2905c6
Ivan Zabrodin <pyrat32@gmail.com>
2021-06-06 14:06:35 -0500

    fixing

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 let express = require('express')
 let mongoose = require('mongoose')
 let app = express()
+let dotenv = require('dotenv').config()
 
-mongoose.connect("mongodb://localhost/books")
+const PORT = process.env.PORT || 3000
+
+mongoose.connect("mongodb+srv://admin:hmGosaFTktuZboLe@cluster0.ljlhu.mongodb.net/hikingpro?retryWrites=true&w=majority")
+
+if (process.env.NODE_ENV == 'production') express.static('client/build')
 
 app.use(
     express.json(),
     express.urlencoded(),
-    express.static('client/build'),
     require('./routes')
 )
 
-app.listen(8000, () => console.log('listening 👂'))
+app.listen(PORT, () => console.log('listening 👂'))
